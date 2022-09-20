a12978695a12fa426e5e84208fe5c3e574bde6a8
Ayeni Oluwaseun <78281232+Oluwaseun-ayeni@users.noreply.github.com>
2022-09-20 20:26:36 +0100

    first commit

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('register/', Register.as_view()),
+    path('login/', Login.as_view()),
+    path('user/', UserView.as_view()),
+    path('logout/', Logout.as_view()),
+    
+]
