cc5411e7354e41888c7878c56bfc91b9a667d261
Ayeni Oluwaseun <78281232+Oluwaseun-ayeni@users.noreply.github.com>
2022-09-26 16:51:39 +0100

    updated

--- auth/urls.py
+++ auth/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls'))
+    path('api/', include('users.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+   
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -23,6 +23,4 @@ class UserSerializer(serializers.ModelSerializer):
 
         
 
-        # extra_kwargs = {
-        #     'password': {write_only:True}
-        # }
+        
